a7440aaef72fc5276e82957d6b3679d3ae1adbaa
Jeramai <j.faber@provrex.nl>
2024-07-29 14:06:06 +0700

    Added favicon

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: process.env.NODE_ENV === "production" ? "/Planet-Crashers" : undefined,
+  reactStrictMode: true,
+};
 
 module.exports = nextConfig
